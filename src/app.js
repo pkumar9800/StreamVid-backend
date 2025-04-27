78a1fad1c12fe9010f048b5601731b13603cb1cc
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-04-28 00:45:52 +0530

    fix user router bugs

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 //import router
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.routes.js'
 
 //declare route
 app.use('/api/v1/users', userRouter)
