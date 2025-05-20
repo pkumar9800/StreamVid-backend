a616044d8437a103a69c432d8a0b7a7e7ad6028c
Pankaj Kumar <174971757+pkumar9800@users.noreply.github.com>
2025-05-20 12:49:53 +0530

    fix bug in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -12,8 +12,6 @@ export const verifyJWT = asyncHandler(async (req, _ , next)=>{
   
     const decodedToken = jwt.verify(token, process.env.ACCESS_KEY_SECRET)
   
-    console.log(decodedToken)
-  
     const user = await User.findById(decodedToken._id).select("-password -refreshToken")
   
     if(!user){
